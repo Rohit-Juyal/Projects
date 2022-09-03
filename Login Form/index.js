bb562fda992befb8e88911b63a61a543bc8ba002
Rohit <rohitjuyal2003@gmail.com>
2022-09-03 23:36:50 +0530

    updated

--- Login Form/index.js
+++ Login Form/index.js
@@ -1,5 +1,5 @@
 const pink = document.getElementById("pink");
-const container = document.getElementById("container"); 
+const container = document.getElementById("container");
 const red = document.getElementById("red");
 const blue = document.getElementById("blue");
 const yellow = document.getElementById("yellow");
@@ -12,44 +12,44 @@ const formInput = document.querySelectorAll(".formInput");
 pink.addEventListener("click", () => {
     container.style.backgroundColor = "rgb(235, 138, 177)";
     document.body.style.backgroundColor = "lightpink";
-    for(let i = 0; i < formContent.length; i++) {
-        formContent[i].style.backgroundColor ="#rgb(207, 38, 78)"
+    for (let i = 0; i < formContent.length; i++) {
+        formContent[i].style.backgroundColor = "#rgb(207, 38, 78)"
         formLabel[i].style.backgroundColor = "rgb(105, 2, 2)";
-    }   
+    }
 })
 
 red.addEventListener("click", () => {
     container.style.backgroundColor = "#b33f32";
     document.body.style.backgroundColor = "#E04961";
-    for(let i = 0; i < formContent.length; i++) {
-        formContent[i].style.backgroundColor ="#E04961"
+    for (let i = 0; i < formContent.length; i++) {
+        formContent[i].style.backgroundColor = "#E04961"
         formLabel[i].style.backgroundColor = "#b82121";
-    }   
+    }
 })
 
 blue.addEventListener("click", () => {
     container.style.backgroundColor = "blue";
     document.body.style.backgroundColor = "lightblue";
-    for(let i = 0; i < formContent.length; i++) {
-        formContent[i].style.backgroundColor ="#6a8fde"
+    for (let i = 0; i < formContent.length; i++) {
+        formContent[i].style.backgroundColor = "#6a8fde"
         formLabel[i].style.backgroundColor = "#201d54";
-    }    
+    }
 })
 
 yellow.addEventListener("click", () => {
     container.style.backgroundColor = "yellow";
     document.body.style.backgroundColor = "#FAFE7D";
-    for(let i = 0; i < formContent.length; i++) {
-        formContent[i].style.backgroundColor ="#edae40"
+    for (let i = 0; i < formContent.length; i++) {
+        formContent[i].style.backgroundColor = "#edae40"
         formLabel[i].style.backgroundColor = "#c98002";
-    }   
+    }
 })
 
 green.addEventListener("click", () => {
     container.style.backgroundColor = "green";
     document.body.style.backgroundColor = "lightgreen";
-    for(let i = 0; i < formContent.length; i++) {
-        formContent[i].style.backgroundColor ="#235221";
+    for (let i = 0; i < formContent.length; i++) {
+        formContent[i].style.backgroundColor = "#235221";
         formLabel[i].style.backgroundColor = "#0a3608";
-    }   
+    }
 })
