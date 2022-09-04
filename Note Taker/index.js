9d2e98f2832fedada8366139b2ac28c1c5ec9bf5
Rohit <rohitjuyal2003@gmail.com>
2022-09-04 13:18:25 +0530

    updated

--- Note Taker/index.js
+++ Note Taker/index.js
@@ -1,37 +1,52 @@
 window.addEventListener("load", () => {
     const form = document.getElementById("note-form");
-    const noteInput = document.getElementById("note-input");
-    const submit = document.getElementById("submit");
-    const notes = document.getElementById("notes");
+    const noteTitle = document.querySelector("#note-title")
+    const noteContent = document.querySelector("#note-content");
+    const noteList = document.querySelector(".note-list");
+    const deleteBtn = document.querySelector("#delete-btn");
 
     form.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        const noteValue = noteInput.value;
+        const notes = document.createElement("div");
+        notes.classList.add("notes")
 
+        const noteItem = document.createElement("div");
+        noteItem.classList.add("note-item");
+        notes.appendChild(noteItem);
 
-        const noteEl = document.createElement("div");
-        noteEl.classList.add("noteEl");
-
-
+        const noteItemTitle = document.createElement("h3");
+        noteItemTitle.classList.add("note-item-title");
+        noteItemTitle.innerHTML = noteTitle.value;
+        noteItem.appendChild(noteItemTitle);
         
-        const noteNumber = document.createElement("div");
-        noteNumber.classList.add("noteNumber");
-        
+        const noteItemContent = document.createElement("p");
+        noteItemContent.classList.add("note-item-content");
+        noteItemContent.innerHTML = noteContent.value;
+        noteItem.appendChild(noteItemContent);
 
-    const inputNoteEl = document.createElement("input")
-    inputNoteEl.classList.add("inputNoteEl");
-    inputNoteEl.type = "text";
-    inputNoteEl.value = noteValue;
-    inputNoteEl.setAttribute("readonly", "readonly");
-    noteEl.appendChild(inputNoteEl);
+        const removeBtn = document.createElement("button");
+        removeBtn.classList.add("btn");
+        removeBtn.classList.add("removeBtn");
+        removeBtn.innerHTML = "Remove";
+        noteItem.appendChild(removeBtn);
+
+        noteList.appendChild(notes);
     
-    inputNoteEl.appendChild(noteNumber);
-        notes.appendChild(noteEl);
+        removeBtn.addEventListener("click", () => {
+            noteItem.remove();
+        });    
+        
+        deleteBtn.addEventListener("click", () => {
+            notes.remove();
+        })
+
+        noteTitle.value = "";
+        noteContent.value = "";
 
     })
 
-    
+
 })
 
 
