af24529d3a39227858a15660a0144f94cb9c6d1c
Rohit <rohitjuyal2003@gmail.com>
2022-10-21 20:09:17 +0530

    added

---
+++ Weather App/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "weather": "url('https://source.unsplash.com/random/1600×900/?weather')"
+      }
+    },
+  },
+  plugins: [],
+}
